3c596ff2ef688e2ee6a62c60adc07762d7792091
Teashrock <kajitsu22@gmail.com>
2026-07-08 22:58:55 +0300

    Fix

--- app/main.py
+++ app/main.py
@@ -5,13 +5,17 @@ def move_file(cmd: str) -> None:
     cmd_split = cmd.split(" ")
     if len(cmd_split) != 3:
         return
-    if cmd_split[0] != "mv":
+    command, source, destination = cmd_split
+    if command != "mv":
         return
-    path = "/".join(cmd_split[2].split("/")[:-1])
+    if destination.endswith("/"):
+        destination += source
+    path = ""
+    if len(destination.split("/")) > 1:
+        path = os.path.join(*destination.split("/")[:-1])
     if path and not os.path.exists(path):
         os.makedirs(path)
-    print(cmd_split[2])
-    with open(cmd_split[1], "r") as inf:
-        with open(f"{cmd_split[2]}", "w") as of:
+    with open(source, "r") as inf:
+        with open(f"{destination}", "w") as of:
             of.write(inf.read())
-    os.remove(cmd_split[1])
+    os.remove(source)
